0cfb553de73c02a53866c774fadbaef9bedabcdc
Unabatedlynx5 <frankrobertdenton@gmail.com>
2022-08-03 22:06:56 -0500

    More progress: Button turn background green.

---
+++ background.js
@@ -0,0 +1,8 @@
+// background.js
+// sets default background color to green
+let color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
